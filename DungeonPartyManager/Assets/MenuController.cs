349ecd2b03daf03a981dd70ff07fb6bbbe8f2d7b
Nikos Giachoudis <elpaxoudis@gmail.com>
2017-09-05 03:00:00 +0300

    first commit

---
+++ DungeonPartyManager/Assets/MenuController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour {
+
+    public PlayerStatsController characterController;
+
+    public GameObject characterStatsPanelGO;
+
+    public void OpenCharacterStatsPanel()
+    {
+        characterStatsPanelGO.SetActive(true);
+        //characterController.UnpackStats(characterStatsPanelGO);
+    }
+
+    public void CloseCharacterStatsPanel()
+    {
+        characterStatsPanelGO.SetActive(false);
+    }
+
+    private void Start()
+    {
+        //if (characterStatsPanelGO == null)
+        //    Debug.Log("No character stats panel object found!!");
+        //else
+        //    characterController.UnpackStats(characterStatsPanelGO);
+    }
+}
